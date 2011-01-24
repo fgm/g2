519a7974f2407e4f1055246717302229f0c55436
Frederic G. MARAND <fgm@osinet.fr>
2011-01-24 22:10:31 +0000

    Main page converted to template g2_main.
    - theme directory and theme/theme.inc created
    - g2_main.tpl.php created

---
+++ theme/g2_main.tpl.php
@@ -0,0 +1,13 @@
+<?php
+// $Id$
+/**
+ * @file
+ * Template for G2 main page.
+ *
+ * - $alphabar: the configured alphabar
+ * - $text: the text used to build the page
+ */
+?>
+<p><?php echo $alphabar; ?></p><?php
+echo $text;
+?><p><?php if (!empty($text)) echo $alphabar; ?></p>
